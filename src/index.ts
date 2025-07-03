43c3ee967c511e21efadab7832941a7c0915128e
aboveStars <yunuskorkmazis384@outlook.com>
2025-07-03 13:16:38 +0300

    Added compability for require type import.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export { Anomaly as default } from "./middleware";
+import { Anomaly } from "./middleware";
+
+// ES Module default export
+export default Anomaly;
+
+// CommonJS compatibility
+module.exports = Anomaly;
+module.exports.default = Anomaly;
+module.exports.Anomaly = Anomaly;
