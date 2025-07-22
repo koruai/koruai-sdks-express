7a04676a95bf60b85a1153c3226df38120391f1a
aboveStars <yunuskorkmazis384@outlook.com>
2025-07-22 23:27:58 +0300

    Added production urls.

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,14 +5,14 @@ const environment: "production" | "development" | "local_development" =
 
 export const REQUEST_COLLECTION_ENDPOINT =
   environment === "production"
-    ? "https://anomaly-request-collector-api-233311529656.europe-west1.run.app/collect-request"
+    ? "https://request-colletor-api-973817945996.europe-west1.run.app/collect-request"
     : environment === "development"
     ? "https://request-colletor-api-229153978352.europe-west1.run.app/collect-request"
     : "http://localhost:3001/collect-request";
 
 export const FETCH_POLICIES_ENDPOINT =
   environment === "production"
-    ? "https://anomaly-read-clickhouse-api-233311529656.europe-west1.run.app/get-policies"
+    ? "https://clickhouse-reader-api-973817945996.europe-west1.run.app/get-policies"
     : environment === "development"
     ? "https://clickhouse-reader-api-229153978352.europe-west1.run.app/get-policies"
     : "http://localhost:3002/get-policies";
